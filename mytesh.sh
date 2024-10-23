d5abc84158d5e9772fc144782144fb64261e6d9f
Jainkvta <jainkvta123@gmail.com>
2024-10-23 11:06:09 -0700

    Update mytesh.sh

--- mytesh.sh
+++ mytesh.sh
@@ -1,19 +1,2 @@
  echo "hello world"
-date
-cal 
-echo "hey"
-echo "hello"
-echo "pipe"
-echo "jj"
-echo "DFD"
-echo "fhxdfjk"
-echo "FDdf"
-echo "dfghjk"
-cal
-date
-echo "Fdf"
-echo " fdsf"
-cal
-date
-echo "fg"
-date
+echo "fdf"
